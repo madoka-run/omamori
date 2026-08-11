8590d25a7704bb49bfdd74b65d3a249d604f4d2d
Madoka <maachame@gmail.com>
2026-08-12 01:20:56 +0900

    feat: 呼吸法後の確認画面と分岐フローを追加

--- app/controllers/anxiety_logs_controller.rb
+++ app/controllers/anxiety_logs_controller.rb
@@ -3,12 +3,30 @@ class AnxietyLogsController < ApplicationController
   end
 
   def create
-    AnxietyLog.create(anxiety_level: params[:anxiety_level], started_at: Time.current)
-    redirect_to breathing_path("calming")
+    anxiety_log = AnxietyLog.create(anxiety_level: params[:anxiety_level], started_at: Time.current)
+    redirect_to breathing_path("calming", anxiety_log_id: anxiety_log.id)
   end
 
-  def check 
+  def check
+    @anxiety_log = AnxietyLog.find(params[:id])
+  end
+
+  def update
+    @anxiety_log = AnxietyLog.find(params[:id])
+
+    if params[:status] == "resolved"
+      @anxiety_log.update(ended_at: Time.current)
+      redirect_to finished_anxiety_log_path(@anxiety_log)
+    else
+      redirect_to suggestions_anxiety_log_path(@anxiety_log)
+    end
+  end
+
+  def finished
+    @anxiety_log = AnxietyLog.find(params[:id])
+  end
+
+  def suggestions
     @anxiety_log = AnxietyLog.find(params[:id])
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :anxiety_logs do
     member do
       get :check
+      get :finished
+      get :suggestions
     end
   end
 
