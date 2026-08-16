95d0d59f98cd0f0ac733d52056abc62ddd4dbcdb
Madoka <maachame@gmail.com>
2026-08-16 23:24:25 +0900

    feat: suggestionsのカードを2列グリッド+ポップアップ表示に変更

--- app/javascript/controllers/card_controller.js
+++ app/javascript/controllers/card_controller.js
@@ -1,9 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["detail"]
+    static targets = ["detail", "backdrop"]
 
-    toggle() {
-        this.detailTarget.hidden = !this.detailTarget.hidden
+    toggle(event) {
+        const clickedDetail = event.currentTarget.nextElementSibling
+        const willOpen = clickedDetail.hidden
+
+        this.detailTargets.forEach((detail) => {
+            detail.hidden = detail === clickedDetail ? !detail.hidden : true
+        })
+
+        this.backdropTarget.hidden = !willOpen
+    }
+
+    closeAll() {
+        this.detailTargets.forEach((detail) => { detail.hidden = true })
+        this.backdropTarget.hidden = true
     }
 }
+
